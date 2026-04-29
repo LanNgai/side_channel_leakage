0c673bd8652ba472bdc7a1a61f021849d44da57c
Lan Ngai <b00159127@mytudublin.ie>
2026-04-29 23:05:55 +0100

    Made basic_pyshark.py work
    - Version mismatch, created a virtual environment with python 3.12.
    
    - loop error fixed
    - specifically filters for probe requests and responses

--- basic_pyshark.py
+++ basic_pyshark.py
@@ -1,7 +1,22 @@
 # https://medium.com/@ahmedsobhialii/deep-packet-capture-in-python-a-complete-guide-to-sniffing-techniques-54d650e403e1
-
+import asyncio
 import pyshark
 
-cap = pyshark.LiveCapture(interface='eth0')
-for packet in cap.sniff_continuously(packet_count=10):
-    print(packet)
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+cap = pyshark.LiveCapture(interface='wlp0s20f3',display_filter='wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05')
+
+for packet in cap.sniff_continuously():
+    try: 
+        print("Scanning.......")
+        mac = packet.wlan.sa
+        ssid = packet.wlan_mgt.ssid
+        ssid_str = "".join("%s" % k for k in ssid)
+        rssi = packet.radiotap.dbm_antsignal
+        
+        print("MAC: %c | SSID: %s | Signal Strength: %d dBm" % (mac, ssid_str, rssi))
+        
+    except AttributeError:
+        pass
