b67273ac4bbb2b1c0a7773b68aa6acd9c07ebe27
Lan Ngai <b00159127@mytudublin.ie>
2026-04-30 03:46:59 +0100

    Update basic_pyshark.py
    - added distance stuff

--- basic_pyshark.py
+++ basic_pyshark.py
@@ -6,7 +6,13 @@ import pyshark
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
-cap = pyshark.LiveCapture(interface='wlp0s20f0u1u1',display_filter='(wlan.fc.type_subtype == 0x00 || wlan.fc.type_subtype == 0x01 || wlan.fc.type_subtype == 0x02 || wlan.fc.type_subtype == 0x03 ||  wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05)')
+cap = pyshark.LiveCapture(interface='(wlp0s20f0u1u4u1)', display_filter='(wlan.fc.type_subtype == 0x00 || wlan.fc.type_subtype == 0x01 || wlan.fc.type_subtype == 0x02 || wlan.fc.type_subtype == 0x03 ||  wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05)')
+
+calibration = -44 # calibration device's RSS at a distance of 1 meter                              
+condition = 2.5 # accuracy notes
+
+def rssi_to_distance(rssi):
+    return round(10 ** ((calibration - rssi) / (10 * condition)), 2)
 
 for packet in cap.sniff_continuously():
  
@@ -14,6 +20,7 @@ for packet in cap.sniff_continuously():
     ssid = packet.wlan.get_field_value('ssid') # ssid field
     bssid = packet.wlan.bssid
     rssi = int(packet.radiotap.dbm_antsignal)
+    distance = rssi_to_distance(rssi)
     
-    print(f"MAC: {mac} | SSID: '{ssid}' |  BSSID: '{bssid}' | Signal: {rssi} dBm")
+    print(f"MAC: {mac} | SSID: '{ssid}' |  BSSID: '{bssid}' | Signal: {rssi} dBm | Distance: {distance} m")
 
