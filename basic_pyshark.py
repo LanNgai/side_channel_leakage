cbbac5a03d990115b949aba8a699acbc4341349a
Lan Ngai <b00159127@mytudublin.ie>
2026-04-30 03:14:37 +0100

    Testing pyshark
    - Produces output correctly.
    - 'very_basic.py' just used for testing and getting available fields.

--- basic_pyshark.py
+++ basic_pyshark.py
@@ -1,4 +1,4 @@
-# https://medium.com/@ahmedsobhialii/deep-packet-capture-in-python-a-complete-guide-to-sniffing-techniques-54d650e403e1
+## https://medium.com/@ahmedsobhialii/deep-packet-capture-in-python-a-complete-guide-to-sniffing-techniques-54d650e403e1
 import asyncio
 import pyshark
 
@@ -6,17 +6,14 @@ import pyshark
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
-cap = pyshark.LiveCapture(interface='wlp0s20f3',display_filter='wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05')
+cap = pyshark.LiveCapture(interface='wlp0s20f0u1u1',display_filter='(wlan.fc.type_subtype == 0x00 || wlan.fc.type_subtype == 0x01 || wlan.fc.type_subtype == 0x02 || wlan.fc.type_subtype == 0x03 ||  wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05)')
 
 for packet in cap.sniff_continuously():
-    try: 
-        print("Scanning.......")
-        mac = packet.wlan.sa
-        ssid = packet.wlan_mgt.ssid
-        ssid_str = "".join("%s" % k for k in ssid)
-        rssi = packet.radiotap.dbm_antsignal
-        
-        print("MAC: %c | SSID: %s | Signal Strength: %d dBm" % (mac, ssid_str, rssi))
-        
-    except AttributeError:
-        pass
+ 
+    mac = packet.wlan.sa # source MAC address
+    ssid = packet.wlan.get_field_value('ssid') # ssid field
+    bssid = packet.wlan.bssid
+    rssi = int(packet.radiotap.dbm_antsignal)
+    
+    print(f"MAC: {mac} | SSID: '{ssid}' |  BSSID: '{bssid}' | Signal: {rssi} dBm")
+
