aeb98cc08465086028c225166b467244b83e65e9
Lan Ngai <b00159127@mytudublin.ie>
2026-04-30 04:52:56 +0100

    Making it look pretty

--- basic_pyshark.py
+++ basic_pyshark.py
@@ -2,25 +2,49 @@
 import asyncio
 import pyshark
 
-
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-
-cap = pyshark.LiveCapture(interface='(wlp0s20f0u1u4u1)', display_filter='(wlan.fc.type_subtype == 0x00 || wlan.fc.type_subtype == 0x01 || wlan.fc.type_subtype == 0x02 || wlan.fc.type_subtype == 0x03 ||  wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05)')
-
-calibration = -44 # calibration device's RSS at a distance of 1 meter                              
-condition = 2.5 # accuracy notes
-
-def rssi_to_distance(rssi):
-    return round(10 ** ((calibration - rssi) / (10 * condition)), 2)
-
-for packet in cap.sniff_continuously():
- 
-    mac = packet.wlan.sa # source MAC address
-    ssid = packet.wlan.get_field_value('ssid') # ssid field
-    bssid = packet.wlan.bssid
-    rssi = int(packet.radiotap.dbm_antsignal)
-    distance = rssi_to_distance(rssi)
+class Pyshark_distance():
     
-    print(f"MAC: {mac} | SSID: '{ssid}' |  BSSID: '{bssid}' | Signal: {rssi} dBm | Distance: {distance} m")
+    
+    def user_input(interface):
+        print()
+        # wlp0s20f0u1u4u1
+        # wlp0s20f0u1u1
+        interface = input("Please enter the name of the interface: ")
+        return interface
+    
+    def rssi_to_distance(self, rssi):
+        calibration = -44 # calibration device's RSS at a distance of 1 meter                              
+        condition = 2.5 # accuracy notes
+        return round(10 ** ((calibration - rssi) / (10 * condition)), 2)
+    
+    def live_data(self, interface):
+    
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
 
+        cap = pyshark.LiveCapture(interface= interface, display_filter='(wlan.fc.type_subtype == 0x00 || wlan.fc.type_subtype == 0x01 || wlan.fc.type_subtype == 0x02 || wlan.fc.type_subtype == 0x03 ||  wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05 || wlan.fc.type_subtype == 0x08)', debug=True)
+
+        for packet in cap.sniff_continuously():
+        
+            mac = packet.wlan.sa # source MAC address
+            
+            ssid = packet.wlan.get_field_value('Ssid') # ssid field
+            # ssid = str(packet['wlan_mgt'].get_field_value('ssid') or '(wildcard)')
+            # ssid = str(packet.wlan.mgt.ssid)
+            
+            bssid = packet.wlan.bssid
+            rssi = int(packet.radiotap.dbm_antsignal)
+
+            distance = self.rssi_to_distance(rssi)
+
+            print(f"MAC: {mac} | SSID: '{ssid}' |  BSSID: '{bssid}' | Signal: {rssi} dBm | Distance: {distance} m")
+        
+        
+   
+
+   
+
+if __name__ == '__main__':
+    pd = Pyshark_distance()
+    interface = pd.user_input()
+    pd.live_data(interface)
