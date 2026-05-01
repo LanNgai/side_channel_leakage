da5dffa7e79900d10bb0b60ad39028db13203039
Lan Ngai <b00159127@mytudublin.ie>
2026-05-01 20:38:28 +0100

    ssid debug 1
    Debugged and found that what we thought was named 'ssid' is actually 'wlan_ssid' in our particular version of tshark

--- very_basic.py
+++ very_basic.py
@@ -5,12 +5,17 @@ import pyshark
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
-cap = pyshark.LiveCapture(interface='wlp0s20f0u1u1u4',display_filter='(wlan.fc.type_subtype == 0x00 || wlan.fc.type_subtype == 0x01 || wlan.fc.type_subtype == 0x02 || wlan.fc.type_subtype == 0x03 ||  wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05)', debug=True)
+cap = pyshark.LiveCapture(interface='wlp0s20f0u3u1u4',display_filter='(wlan.fc.type_subtype == 0x00 || wlan.fc.type_subtype == 0x01 || wlan.fc.type_subtype == 0x02 || wlan.fc.type_subtype == 0x03 ||  wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05)', debug=True)
+
+#for packet in cap.sniff_continuously():
+ #   print(packet.wlan.field_names)  # see all available wlan fields
+  #  break
 
 for packet in cap.sniff_continuously():
-    print(packet.wlan.field_names)  # see all available wlan fields
-    break
-
+	for layer in packet.layers:
+		print(f"\n-- {layer.layer_name} ---")
+		print(layer.field_names)
+	break
 
 
 
