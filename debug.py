0c81c8e5c24c52046d39e14073c9c5d7fa380469
Lan Ngai <b00159127@mytudublin.ie>
2026-05-04 19:15:18 +0100

    Update debug.py
    Changed display filter value to be more readable.

--- debug.py
+++ debug.py
@@ -8,7 +8,14 @@ int_one = 'wlp0s20f0u1u1'
 int_two = 'wlp0s20f0u1u4u1'
 int_three = 'wlp0s20f0u3u1u4'
 
-cap = pyshark.LiveCapture(interface= int_three, display_filter='(wlan.fc.type_subtype == 0x00 || wlan.fc.type_subtype == 0x01 || wlan.fc.type_subtype == 0x02 || wlan.fc.type_subtype == 0x03 ||  wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05)', debug=True)
+cap = pyshark.LiveCapture(interface= int_three, 
+						  display_filter='(wlan.fc.type_subtype == 0x00 ' +
+						  '|| wlan.fc.type_subtype == 0x01 ' +
+						  '|| wlan.fc.type_subtype == 0x02 ' +
+						  '|| wlan.fc.type_subtype == 0x03 ' +
+						  '|| wlan.fc.type_subtype == 0x04 ' +
+						  '|| wlan.fc.type_subtype == 0x05)',
+						    debug=True)
 
 def debug_wlan():
 	for packet in cap.sniff_continuously():
