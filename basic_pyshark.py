80f967a002a938e0b37a9927554668f9caf83eac
Lan Ngai <b00159127@mytudublin.ie>
2026-05-04 19:01:26 +0100

    Made the display filter value easier to look at.

--- basic_pyshark.py
+++ basic_pyshark.py
@@ -6,7 +6,13 @@ import pyshark
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
-cap = pyshark.LiveCapture(interface='wlp0s20f0u1u1',display_filter='(wlan.fc.type_subtype == 0x00 || wlan.fc.type_subtype == 0x01 || wlan.fc.type_subtype == 0x02 || wlan.fc.type_subtype == 0x03 ||  wlan.fc.type_subtype == 0x04 || wlan.fc.type_subtype == 0x05)')
+cap = pyshark.LiveCapture(interface='wlp0s20f0u1u1',
+                          display_filter='(wlan.fc.type_subtype == 0x00 ' +
+                          '|| wlan.fc.type_subtype == 0x01 ' +
+                          '|| wlan.fc.type_subtype == 0x02 ' +
+                          '|| wlan.fc.type_subtype == 0x03 ' +
+                          '|| wlan.fc.type_subtype == 0x04 ' +
+                          '|| wlan.fc.type_subtype == 0x05)')
 
 for packet in cap.sniff_continuously():
  
